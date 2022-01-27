e298400f2dacf42d89dae26d604690d846618561
HH Mocumbi <heliomucumbi@gmail.com>
2022-01-26 22:52:25 -0800

    Actualização do site comissão de exames de admissão - up

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>UP-VITRINE</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no" > 
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
+
+    <body>
+                
+<nav class="navbar navbar-expand-lg navbar-light headerBackground">
+  <a class="navbar-brand ml-5" href="#"><img src="assets/img/images/logoup.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse mr-5 " id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Edital 2022</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Inscrições 2022
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Efectuar Cadastro</a>
+          <a class="dropdown-item" href="#">Entrar na plataforma</a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Links Úteis
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Portal da UP</a>
+          <a class="dropdown-item" href="#">Portal do Estudante</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Normas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="#">Contacte-nos</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<main class="container my-5">
+    <div class="row bg-light box-shadow border-radius">
+        <div class="col-12">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="assets/img/slides/nivo/adm20218.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="assets/img/slides/nivo/adm20216.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="assets/img/slides/nivo/adm20211.jpg" alt="Third slide">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+            </div>
+        </div>
+    </div>
+</main>
+            <script type="text/javascript" src="assets/js/jquery-3.2.1.slim.min.js"></script>
+            <script type="text/javascript" src="assets/js/popper.min.js"></script>
+            <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    </body>
+</html>
+
